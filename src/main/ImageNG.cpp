00d516946461723856be7fd9df0ee9771146b994
Julienraptor01 <julienraptor01@gmail.com>
2023-09-25 14:44:28 +0200

    add the base code

---
+++ src/main/ImageNG.cpp
@@ -0,0 +1,32 @@
+#include "ImageNG.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+ImageNG::ImageNG()
+{
+#ifdef DEBUG
+	cout<<"Constructeur par défaut de ImageNG"<<endl;
+#endif
+
+	id=1;
+	//nom=NULL;
+	nom=(char *)malloc(20);
+	strcpy(nom,"ImageNG sans nom");
+}
+
+ImageNG::~ImageNG()
+{
+#ifdef DEBUG
+	cout<<"Destructeur de ImageNG"<<endl;
+#endif
+
+	if(nom !=NULL)
+		free(nom);
+}
+
+void ImageNG::Affiche()
+{
+	cout<<"id="<<id<<" nom="<<nom<<endl;
+}
---
+++ src/main/ImageNG.h
@@ -0,0 +1,17 @@
+#ifndef IMAGENG_H
+#define IMAGENG_H
+
+class ImageNG
+{
+private:
+	int id;
+	char * nom;
+
+public:
+	ImageNG();
+	~ImageNG();
+
+	void Affiche();
+};
+
+#endif
