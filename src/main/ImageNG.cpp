350dbc679cee3e2cc4b0e09bacae288eac800ade
Julienraptor01 <julienraptor01@gmail.com>
2023-10-09 13:35:30 +0200

    reformat my code

--- src/main/ImageNG.cpp
+++ src/main/ImageNG.cpp
@@ -1,15 +1,15 @@
-#include "ImageNG.h"
 #include <iostream>
 #include <string.h>
 
 using namespace std;
+
+#include "ImageNG.h"
 
 ImageNG::ImageNG()
 {
 #ifdef DEBUG
 	cout<<"Constructeur par défaut de ImageNG"<<endl;
 #endif
-
 	id=1;
 	//nom=NULL;
 	nom=(char *)malloc(20);
@@ -21,7 +21,6 @@ ImageNG::~ImageNG()
 #ifdef DEBUG
 	cout<<"Destructeur de ImageNG"<<endl;
 #endif
-
 	if(nom !=NULL)
 		free(nom);
 }
