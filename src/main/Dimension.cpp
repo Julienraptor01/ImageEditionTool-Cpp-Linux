6b8c69cbbb03be4604873b82fcc4bf71c2469590
Julienraptor01 <julienraptor01@gmail.com>
2023-10-19 20:17:20 +0200

    improve the debug logging

--- src/main/Dimension.cpp
+++ src/main/Dimension.cpp
@@ -8,41 +8,53 @@ using namespace std;
 Dimension::Dimension()
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur par défaut de Dimension\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur par défaut de Dimension\033[0m"<<endl;
 #endif
 	setLargeur(DIMENSION_H_DEFAULT_LARGEUR);
 	setHauteur(DIMENSION_H_DEFAULT_HAUTEUR);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur par défaut de Dimension\033[0m"<<endl;
+#endif
 }
 
 Dimension::~Dimension()
 {
 #ifdef DEBUG
-	cout<<"\033[41mDEBUG : Destructeur de Dimension\033[49m"<<endl;
+	cout<<"\033[32;41mDEBUG : Destructeur de Dimension\033[0m"<<endl;
+#endif
+#ifdef DEBUG
+	cout<<"\033[34;41mDEBUG : Fin Destructeur de Dimension\033[0m"<<endl;
 #endif
 }
 
 Dimension::Dimension(int largeur, int hauteur)
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur d'initialisation complet de Dimension\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur d'initialisation complet de Dimension\033[0m"<<endl;
 #endif
 	setLargeur(largeur);
 	setHauteur(hauteur);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur d'initialisation complet de Dimension\033[0m"<<endl;
+#endif
 }
 
 Dimension::Dimension(const Dimension &dimension)
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur de copie de Dimension\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur de copie de Dimension\033[0m"<<endl;
 #endif
 	setLargeur(dimension.largeur);
 	setHauteur(dimension.hauteur);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur de copie de Dimension\033[0m"<<endl;
+#endif
 }
 
 void Dimension::setLargeur(int largeur)
 {
 #ifdef DEBUGVERYVERBOSE
-	cout<<"\033[44mDEBUGVERYVERBOSE : setLargeur de Dimension\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERYVERBOSE : setLargeur de Dimension\033[0m"<<endl;
 #endif
 	if (largeur<=0)
 		largeur=DIMENSION_H_DEFAULT_LARGEUR;
@@ -52,7 +64,7 @@ void Dimension::setLargeur(int largeur)
 int Dimension::getLargeur()const
 {
 #ifdef DEBUGVERYVERBOSE
-	cout<<"\033[44mDEBUGVERYVERBOSE : getLargeur de Dimension\033[49m"<<endl;
+	cout<<"\033[32;44mDEBUGVERYVERBOSE : getLargeur de Dimension\033[0m"<<endl;
 #endif
 	return largeur;
 }
@@ -60,7 +72,7 @@ int Dimension::getLargeur()const
 void Dimension::setHauteur(int hauteur)
 {
 #ifdef DEBUGVERYVERBOSE
-	cout<<"\033[44mDEBUGVERYVERBOSE : setHauteur de Dimension\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERYVERBOSE : setHauteur de Dimension\033[0m"<<endl;
 #endif
 	if (hauteur<=0)
 		hauteur=DIMENSION_H_DEFAULT_HAUTEUR;
@@ -70,7 +82,7 @@ void Dimension::setHauteur(int hauteur)
 int Dimension::getHauteur()const
 {
 #ifdef DEBUGVERYVERBOSE
-	cout<<"\033[44mDEBUGVERYVERBOSE : getHauteur de Dimension\033[49m"<<endl;
+	cout<<"\033[32;44mDEBUGVERYVERBOSE : getHauteur de Dimension\033[0m"<<endl;
 #endif
 	return hauteur;
 }
--- src/main/ImageNG.cpp
+++ src/main/ImageNG.cpp
@@ -10,16 +10,19 @@ using namespace std;
 void ImageNG::freeNom()
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[43mDEBUGVERBOSE : freeNom de ImageNG\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : freeNom de ImageNG\033[0m"<<endl;
 #endif
 	free(nom);
 	nom=NULL;
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin freeNom de ImageNG\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::createMatrice()
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[43mDEBUGVERBOSE : createMatrice de ImageNG\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : createMatrice de ImageNG\033[0m"<<endl;
 #endif
 	matrice = new int*[dimension.getLargeur()];
 	for(int i=0;i<dimension.getLargeur();i++)
@@ -28,95 +31,122 @@ void ImageNG::createMatrice()
 		for(int j=0;j<dimension.getHauteur();j++)
 			matrice[i][j] = 0;
 	}
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin createMatrice de ImageNG\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::freeMatrice(int **matrice, const Dimension &dimension)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[43mDEBUGVERBOSE : freeMatrice de ImageNG (matrice)\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : freeMatrice de ImageNG (matrice, dimension)\033[0m"<<endl;
 #endif
 	for(int i=0;i<dimension.getLargeur();i++)
 		delete[] matrice[i];
 	delete[] matrice;
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin freeMatrice de ImageNG (matrice, dimension)\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::freeMatrice()
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[43mDEBUGVERBOSE : freeMatrice de ImageNG\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : freeMatrice de ImageNG\033[0m"<<endl;
 #endif
 	freeMatrice(matrice, dimension);
 	matrice = nullptr;
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin freeMatrice de ImageNG\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::copyMatrice(int **matrice, const Dimension &dimension, int **matriceToCopy, const Dimension &dimensionOfMatriceToCopy)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[43mDEBUGVERBOSE : copyMatrice de ImageNG (matrice, dimension, matriceToCopy, dimensionOfMatriceToCopy)\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : copyMatrice de ImageNG\033[0m"<<endl;
 #endif
 	int minLargeur = (dimension.getLargeur() < dimensionOfMatriceToCopy.getLargeur()) ? dimension.getLargeur() : dimensionOfMatriceToCopy.getLargeur();
 	int minHauteur = (dimension.getHauteur() < dimensionOfMatriceToCopy.getHauteur()) ? dimension.getHauteur() : dimensionOfMatriceToCopy.getHauteur();
 	for(int i=0;i<minLargeur;i++)
 		for(int j=0;j<minHauteur;j++)
 			matrice[i][j] = matriceToCopy[i][j];
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin copyMatrice de ImageNG\033[0m"<<endl;
+#endif
 }
 
 ImageNG::ImageNG()
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur par défaut de ImageNG\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur par défaut de ImageNG\033[0m"<<endl;
 #endif
 	setId(1);
 	setNom("ImageNG sans nom");
 	setDimension(dimension);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur par défaut de ImageNG\033[0m"<<endl;
+#endif
 }
 
 ImageNG::~ImageNG()
 {
 #ifdef DEBUG
-	cout<<"\033[41mDEBUG : Destructeur de ImageNG\033[49m"<<endl;
+	cout<<"\033[32;41mDEBUG : Destructeur de ImageNG\033[0m"<<endl;
 #endif
 	if(nom != NULL)
 		freeNom();
 	if(matrice != nullptr)
 		freeMatrice();
+#ifdef DEBUG
+	cout<<"\033[34;41mDEBUG : Fin Destructeur de ImageNG\033[0m"<<endl;
+#endif
 }
 
 ImageNG::ImageNG(int id, const char *nom, const Dimension &dimension)
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur d'initialisation complet de ImageNG\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur d'initialisation complet de ImageNG\033[0m"<<endl;
 #endif
 	setId(id);
 	setNom(nom);
 	setDimension(dimension);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur d'initialisation complet de ImageNG\033[0m"<<endl;
+#endif
 }
 
 ImageNG::ImageNG(int id, const char *nom)
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur d'initialisation partiel de ImageNG\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur d'initialisation partiel de ImageNG\033[0m"<<endl;
 #endif
 	setId(id);
 	setNom(nom);
 	setDimension(dimension);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur d'initialisation partiel de ImageNG\033[0m"<<endl;
+#endif
 }
 
 ImageNG::ImageNG(const ImageNG &image)
 {
 #ifdef DEBUG
-	cout<<"\033[42mDEBUG : Constructeur de copie de ImageNG\033[49m"<<endl;
+	cout<<"\033[34;42mDEBUG : Constructeur de copie de ImageNG\033[0m"<<endl;
 #endif
 	setId(image.id);
 	setNom(image.nom);
 	setDimension(image.dimension);
 	copyMatrice(matrice,dimension,image.matrice,image.dimension);
+#ifdef DEBUG
+	cout<<"\033[31;42mDEBUG : Fin Constructeur de copie de ImageNG\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::setId(int id)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : setId de ImageNG\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERBOSE : setId de ImageNG\033[0m"<<endl;
 #endif
 	this->id=id;
 }
@@ -124,7 +154,7 @@ void ImageNG::setId(int id)
 int ImageNG::getId()const
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : getId de ImageNG\033[49m"<<endl;
+	cout<<"\033[32;44mDEBUGVERBOSE : getId de ImageNG\033[0m"<<endl;
 #endif
 	return id;
 }
@@ -132,7 +162,7 @@ int ImageNG::getId()const
 void ImageNG::setNom(const char *nom)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : setNom de ImageNG\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERBOSE : setNom de ImageNG\033[0m"<<endl;
 #endif
 	if(nom != NULL)
 		freeNom();
@@ -143,7 +173,7 @@ void ImageNG::setNom(const char *nom)
 char *ImageNG::getNom()const
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : getNom de ImageNG\033[49m"<<endl;
+	cout<<"\033[32;44mDEBUGVERBOSE : getNom de ImageNG\033[0m"<<endl;
 #endif
 	return nom;
 }
@@ -151,7 +181,7 @@ char *ImageNG::getNom()const
 void ImageNG::setDimension(const Dimension &dimension)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : setDimension de ImageNG\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERBOSE : setDimension de ImageNG\033[0m"<<endl;
 #endif
 	if(matrice == nullptr)
 	{
@@ -188,12 +218,15 @@ void ImageNG::setDimension(const Dimension &dimension)
 		//we need to free the old matrix
 		freeMatrice(oldMatrice, oldDimension);
 	}
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;44mDEBUGVERBOSE : Fin setDimension de ImageNG\033[0m"<<endl;
+#endif
 }
 
 Dimension ImageNG::getDimension()const
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : getDimension de ImageNG\033[49m"<<endl;
+	cout<<"\033[32;44mDEBUGVERBOSE : getDimension de ImageNG\033[0m"<<endl;
 #endif
 	return dimension;
 }
@@ -201,7 +234,7 @@ Dimension ImageNG::getDimension()const
 void ImageNG::setBackground(int couleur)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : setBackground de ImageNG\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERBOSE : setBackground de ImageNG\033[0m"<<endl;
 #endif
 	for(int i=0;i<dimension.getLargeur();i++)
 		for(int j=0;j<dimension.getHauteur();j++)
@@ -211,7 +244,7 @@ void ImageNG::setBackground(int couleur)
 void ImageNG::setPixel(int x, int y, int couleur)
 {
 #ifdef DEBUGVERYVERBOSE
-	cout<<"\033[44mDEBUGVERYVERBOSE : setPixel de ImageNG\033[49m"<<endl;
+	cout<<"\033[31;44mDEBUGVERYVERBOSE : setPixel de ImageNG\033[0m"<<endl;
 #endif
 	if(x>=0 && x<dimension.getLargeur() && y>=0 && y<dimension.getHauteur())
 		matrice[x][y] = couleur;
@@ -220,7 +253,7 @@ void ImageNG::setPixel(int x, int y, int couleur)
 int ImageNG::getPixel(int x, int y) const
 {
 #ifdef DEBUGVERYVERBOSE
-	cout<<"\033[44mDEBUGVERYVERBOSE : getPixel de ImageNG\033[49m"<<endl;
+	cout<<"\033[32;44mDEBUGVERYVERBOSE : getPixel de ImageNG\033[0m"<<endl;
 #endif
 	if(x>=0 && x<dimension.getLargeur() && y>=0 && y<dimension.getHauteur())
 		return matrice[x][y];
@@ -235,23 +268,32 @@ void ImageNG::Affiche()const
 void ImageNG::Dessine() const
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : Dessine de ImageNG\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : Dessine de ImageNG\033[0m"<<endl;
 #endif
 	MyQT::ViewImage(*this);
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin Dessine de ImageNG\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::importFromFile(const char *nomFichier)
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : importFromFile de ImageNG\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : importFromFile de ImageNG\033[0m"<<endl;
 #endif
 	MyQT::ImportFromFile(*this, nomFichier);
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin importFromFile de ImageNG\033[0m"<<endl;
+#endif
 }
 
 void ImageNG::exportToFile(const char *nomFichier, const char *formatFichier) const
 {
 #ifdef DEBUGVERBOSE
-	cout<<"\033[44mDEBUGVERBOSE : exportToFile de ImageNG\033[49m"<<endl;
+	cout<<"\033[35;43mDEBUGVERBOSE : exportToFile de ImageNG\033[0m"<<endl;
 #endif
 	MyQT::ExportToFile(*this, nomFichier, formatFichier);
+#ifdef DEBUGVERBOSE
+	cout<<"\033[36;43mDEBUGVERBOSE : Fin exportToFile de ImageNG\033[0m"<<endl;
+#endif
 }
