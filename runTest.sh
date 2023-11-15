9c169443e1c7ba5c8a7de0cc7b5a18eed64b1372
Julienraptor01 <julienraptor01@gmail.com>
2023-11-15 14:51:11 +0100

    enhance the script to run tests

--- runTest.sh
+++ runTest.sh
@@ -6,11 +6,11 @@ if [ -z "$test_number" ]
 then
 	echo "No test number provided"
 	exit -1
-elif ! [[ $test_number =~ ^[0-9]+$ ]] && ! [[ $test_number =~ ^[0-9]+[a-z]$ ]]
+elif ! [[ $test_number =~ ^-?[0-9]+$ ]] && ! [[ $test_number =~ ^-?[0-9]+[a-z]$ ]]
 then
 	echo "Invalid test number"
 	exit -2
-elif [[ $test_number =~ ^[0-9]+[a-z]$ ]] || [ $test_number -gt 9 ]
+elif [[ $test_number =~ ^-?[0-9]+[a-z]$ ]] || [ $test_number -gt 9 ]
 then
 	./build/bin/test/test$test_number
 elif [ $test_number -gt 0 ]
@@ -33,6 +33,32 @@ then
 	./build/bin/test/test04 7 > ./logs/test04-07.log
 	./build/bin/test/test04 8 > ./logs/test04-08.log
 	./build/bin/test/test04 9 > ./logs/test04-09.log
+	./build/bin/test/test04 10 > ./logs/test04-10.log
+	./build/bin/test/test04 11 > ./logs/test04-11.log
+	./build/bin/test/test05 1 > ./logs/test05-1.log
+	./build/bin/test/test05 2 > ./logs/test05-2.log
+	./build/bin/test/test05 3 > ./logs/test05-3.log
+	./build/bin/test/test05 4 > ./logs/test05-4.log
+	./build/bin/test/test05 5 > ./logs/test05-5.log
+	./build/bin/test/test05 6 > ./logs/test05-6.log
+	./build/bin/test/test05 7 > ./logs/test05-7.log
+elif [ $test_number -eq -1 ]
+then
+	./build/bin/test/test01 > ./logs/test01.log
+	./build/bin/test/test02 1 > ./logs/test02-1.log
+	./build/bin/test/test02 2 > ./logs/test02-2.log
+	./build/bin/test/test03 1 > ./logs/test03-1.log
+	./build/bin/test/test03 2 > ./logs/test03-2.log
+	./build/bin/test/test03 3 > ./logs/test03-3.log
+	./build/bin/test/test04 1 > ./logs/test04-01.log
+	./build/bin/test/test04 2 > ./logs/test04-02.log
+	#./build/bin/test/test04 3 > ./logs/test04-03.log
+	./build/bin/test/test04 4 > ./logs/test04-04.log
+	./build/bin/test/test04 5 > ./logs/test04-05.log
+	./build/bin/test/test04 6 > ./logs/test04-06.log
+	./build/bin/test/test04 7 > ./logs/test04-07.log
+	./build/bin/test/test04 8 > ./logs/test04-08.log
+	#./build/bin/test/test04 9 > ./logs/test04-09.log
 	./build/bin/test/test04 10 > ./logs/test04-10.log
 	./build/bin/test/test04 11 > ./logs/test04-11.log
 	./build/bin/test/test05 1 > ./logs/test05-1.log
